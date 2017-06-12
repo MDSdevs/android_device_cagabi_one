4d76f8bf442030968b71aa5f135cd9932b6f96db
FeyoMx <elfeyo1980@gmail.com>
2017-06-12 08:38:06 -0500

    Cagabi-fy
    Cagabi-fy

--- cm.mk
+++ cm.mk
@@ -1,17 +1,17 @@
 ## Specify phone tech before including full_phone
 
 # Release name
-PRODUCT_RELEASE_NAME := x510
+PRODUCT_RELEASE_NAME := one
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/infinix/x510/device_x510.mk)
+$(call inherit-product, device/cagabi/one/device_one.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := x510
-PRODUCT_NAME := cm_x510
-PRODUCT_BRAND := INFINIX
-PRODUCT_MODEL := X510
-PRODUCT_MANUFACTURER := INFINIX
+PRODUCT_DEVICE := one
+PRODUCT_NAME := cm_one
+PRODUCT_BRAND := CAGABI
+PRODUCT_MODEL := ONE
+PRODUCT_MANUFACTURER := CAGABI
