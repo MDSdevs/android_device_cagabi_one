3c1d78ac5c8af94b90a00b1e81893d0403ed65ef
FeyoMx <elfeyo1980@gmail.com>
2017-06-12 08:36:27 -0500

    Cagabi-fy
    Cagabi-fy

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,6 +1,6 @@
 # mt6580 platform boardconfig
-LOCAL_PATH := device/infinix/x510
--include vendor/infinix/x510/BoardConfigVendor.mk
+LOCAL_PATH := device/cagabi/one
+-include vendor/cagabi/one/BoardConfigVendor.mk
 
 TARGET_SPECIFIC_HEADER_PATH := $(LOCAL_PATH)/include
 
@@ -132,7 +132,7 @@ TARGET_USERIMAGES_USE_EXT4 := true
 
 # SELinux
 BOARD_SEPOLICY_DIRS := \
-       device/infinix/x510/sepolicy
+       device/cagabi/one/sepolicy
 
 # Hack for building without kernel sources
 $(shell mkdir -p $(OUT)/obj/KERNEL_OBJ/usr)
